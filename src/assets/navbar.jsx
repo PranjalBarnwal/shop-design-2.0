ace73ff520cb95dd7ee6dd6a1d6d600cbfc24473
Pranjal <happybarnwal007@gmail.com>
2023-02-08 23:59:32 +0530

    navbar

--- src/assets/navbar.jsx
+++ src/assets/navbar.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import './Navbar.css'
-
+import { useState } from 'react';
+import Logo from "../resources/Logo.png";
+import Logo2 from "../resources/Logo2.png";
 function Navbar() {
+  // let h=true;
+  // const[h,seth]=useState(true);
+  // window.addEventListener("scroll",function(){
+  //  if(window.scrollY==0) seth(false);
+  //  else seth(true);
+  //  console.log(h);
+
+  // })
   return (
-  <nav>
+  <nav className='nav'>
     <div className='NavMain'>
-      <div className="logo nav-items"></div>
+      <div className="logo nav-items">
+        <img className='logoimg' src={Logo} alt="logo"  />
+        {/* <img className={`logo2img ${!h?'hide':''}`} src={Logo2} alt="logo"  /> */}
+      </div>
       <div className="middle-nav nav-items"><a href="#">HOME</a></div>
       <div className="middle-nav nav-items"><a href="">PRODUCTS</a></div>
       <div className="middle-nav nav-items"><a href="">ABOUT</a></div>
