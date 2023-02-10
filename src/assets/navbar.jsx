3926e0da0f72037edfe22854827849cc7d99e3a3
Pranjal <happybarnwal007@gmail.com>
2023-02-10 15:09:12 +0530

    navbar responsive bug fixed

--- src/assets/navbar.jsx
+++ src/assets/navbar.jsx
@@ -6,28 +6,29 @@ import CloseIcon from '@mui/icons-material/Close';
 import Logo from "../resources/Logo.png";
 
 function Navbar() {
-  // const [toggle, setToggle] = useState(false);
-  // const toggleFunc = () => {
-  //   setToggle(!toggle);
-  // };
-  const menuBtn=document.querySelector(".menu-open");
+
+  
+ 
+  
+  
   const nav=document.querySelector(".NavMain");
-  menuBtn.addEventListener(onclick,function(){
+  function clickHandler(){
     nav.classList.toggle("active");
-  })
+  
+  }
   return (
     <nav className='nav'>
       <div className="logo">
         <img className='logoimg' src={Logo} alt="logo"  />
       </div>
       <div className='burger'>
-      <div className='menu-open'><MenuIcon/></div>
+      <div className='menu-open' onClick={clickHandler}><MenuIcon/></div>
       <div className='menu-close'><CloseIcon/></div>
       
       </div>
 
     <div className='NavMain active'>
-      <div classname="emptydiv nav-items "></div>
+      <div className="emptydiv nav-items "></div>
       <div className="middle-nav nav-items"><a href="#">HOME</a></div>
       <div className="middle-nav nav-items"><a href="">PRODUCTS</a></div>
       <div className="middle-nav nav-items"><a href="">ABOUT</a></div>
